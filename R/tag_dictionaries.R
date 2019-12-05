4a5ee40547e2f2f3e0cb848f8e215e82c9e4dd58
mbannert <matthias.bannert@gmail.com>
2019-12-06 00:02:10 +0100

    new dictionaries /w initial support for negative lists

--- R/tag_dictionaries.R
+++ R/tag_dictionaries.R
@@ -1,37 +1,338 @@
-#' Regular Expressions to Identify Work Related Ads
-#'
-#' Dictionary for manual adcontent
-#' tag "arbeitsstelle".
-#'
+# Tag Dictionary blueprint
+# might want to add import and export
+# operators for
+# declarative json definitions of dicts.
+# #' Dictionary
+# #' @export
+# tag_dict_ <- function(){
+#   dict <- list()
+#   dict$pos <- list(
+#
+#   )
+#   dict$neg <- list(
+#
+#   )
+#   dict
+# }
+
+
+#' Dictionary Labor
 #' @export
-dict_work <- function(){
-  dictionary(list(
+tag_dict_work <- function(){
+  # Note how phrases are used inside a dictionary.
+  # Dict elements may be converted to quanteda dictionaries
+  # and because those are not bare regexp, we need
+  # to hold expressions containing whitespace
+  # separately, see also ?kwic Note on patterns.
+  dict <- list()
+  dict$pos <- list(
     work = "Arbeit$|arbeiten$|Arbeiter$",
-    position ="Platz als|Anstellung|^Dienst$|^Dienste|einzutreten|unterzukommen|^Lohn|Verdienst",
-    apprentice = "Lehrling|Lehrjung|in die Lehr|Lehrgeld")
+    apprentice = "Lehrling|Lehrjung|in die Lehr|Lehrgeld",
+    position_phrase_1 = "Platz als",
+    position_phrase_2 = "Platz zu erhalten",
+    position ="Anstellung|^Dienst$|^Dienste|einzutreten|unterzukommen|^Lohn|Verdienst"
   )
+  dict
 }
 
-
-#' Regular Expressions to Identify Real Estate Related Ads
-#'
-#' Dictionary for manual adcontent tag "immo".
-#'
+#' Dictionary Bed
 #' @export
-dict_real_estate <- function(){
-  dictionary(list(
+tag_dict_bed <- function(){
+  dict <- list()
+  dict$pos <- list(
+    bed = "[B|b]ett|[B|b]eth|[K|k]orbwag|[W|w]iege"
+  )
+  dict$neg <- list(
+    misc = "Elisabeth|Verschwiegenheit|bettel"
+  )
+  dict
+}
+
+#' Dictionary Real Estate
+#' @export
+tag_dict_real_estate <- function(){
+  dict <- list()
+  dict$pos <- list(
     apartment = "losament|^Zimmer|Wohnung|Kammer|Keller"
-  ))
+  )
+  # dict$neg <- list(
+  #
+  # )
+  dict
 }
 
-#' Get Document Ids by Dictionary
+#' Dictionary
 #' @export
-corpus_id_by_dict <- function(corp, dict,
-                               identifier = "id"){
-  docnames(corp) <- docvars(corp, identifier)
-  kwic_res <- kwic(corp,
-                   pattern = dict,
-                   valuetype = "regex")
-  kwic_res$docname
+tag_dict_lotto <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_misc <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_transport <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_caritas <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_position <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_food <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_animal <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+
+#' Dictionary
+#' @export
+tag_dict_info <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_bling <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_churchseat <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+#' Dictionary
+#' @export
+tag_dict_textile <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_grocery <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_finance <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_print <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_household_goods <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_things <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+#' Dictionary
+#' @export
+tag_dict_ <- function(){
+  dict <- list()
+  dict$pos <- list(
+
+  )
+  dict$neg <- list(
+
+  )
+  dict
+}
+
+
+
+
+
+
+
+
+#' Filter by Dictionary, Return Document IDs
+#' @export
+filter_by_dict <- function(corp, dict, identifier = "id",
+                           ignore.case = TRUE){
+  re_pos <- paste(unlist(dict$pos), collapse = "|")
+
+  tf_pos <- grepl(re_pos, corp$documents$texts,
+                  ignore.case = ignore.case)
+
+  if(!is.null(dict$neg)){
+    re_neg <- paste(unlist(dict$neg), collapse = "|")
+    tf_neg <- !grepl(re_neg, corp$documents$texts,
+                    ignore.case = ignore.case)
+    sel <- tf_pos == tf_neg
+  } else{
+    sel <- tf_pos
+  }
+
+  ids <- docvars(corp, identifier)[sel]
+  ids
+
 }
 
---
+++ demos/sandboxing.R
@@ -0,0 +1,46 @@
+# Load libraries and source functions
+# in pre-package state
+library(readtext)
+library(quanteda)
+library(textcat)
+library(jsonlite)
+library(ggplot2)
+source("R/avis_stop.R")
+source("R/ocr_corrections.R")
+source("R/tag_dictionaries.R")
+source("R/cleaners.R")
+
+
+avis_1834 <- readtext("data/avis_1834.csv",
+                      text_field = "text")
+
+avis_1834$text <- correct_ocr(avis_1834$text)
+
+corpus_1834 <- corpus(avis_1834,
+                      docid_field = "doc_id")
+
+# cats inspiration
+# a <- strsplit(avis_1834$adcontent,",")
+# unique(unlist(a))
+
+xx <- corpus_id_by_dict(corpus_1834, dict_work())
+
+head(docvars(corpus_1834))
+
+debug(filter_by_dict)
+ids_work <- filter_by_dict(corpus_1834, tag_dict_work())
+
+corpus_work <- corpus_subset(corpus_1834,
+                             docvars(corpus_1834,"id") %in% ids_work)
+
+
+ids_beds <- filter_by_dict(corpus_1834, tag_dict_bed())
+corpus_bed <- corpus_subset(corpus_1834,
+                             docvars(corpus_1834,"id") %in%
+                              ids_bed)
+
+
+corpus_work$documents$texts[3]
+
+
+
