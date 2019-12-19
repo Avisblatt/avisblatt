21a8e8452e834287664f15adad1ab815b1c0d9b4
aengel17 <49908904+aengel17@users.noreply.github.com>
2019-12-19 10:26:21 +0100

    Minor update, merge filter now also works with whatever category/filter chosen

--- demos/sandbox_testing_new_dict_entries.R
+++ demos/sandbox_testing_new_dict_entries.R
@@ -13,29 +13,6 @@ source("R/tagfilters_household.R", encoding = "UTF-8")
 source("R/tagfilters_main.R", encoding = "UTF-8")
 source("R/cleaners.R")
 source("R/validate_filters.R")
-
-
-#PICK category and filter by removing # from the pertinent line:
-#category <- "01textilien"; original <- tagfilter_textiles()
-#category <- "02hausrat"; original <- tagfilter_household_goods()
-#category <- "03lebensmittel"; original <- tagfilter_grocery()
-#category <- "04schmuck"; original <- tagfilter_bling()
-#category <- "05drucksachen"; original <- tagfilter_print()
-#category <- "06ding"; original <- tagfilter_things()
-#category <- "07tier"; original <- tagfilter_animal()
-#category <- "08immo"; original <- tagfilter_real_estate()
-#category <- "09kirchenstuhl"; original <- tagfilter_churchseat()
-#category <- "arbeit"; original <- tagfilter_labor()
-category <- "11kost"; original <- tagfilter_board()
-#category <- "12platzierung"; original <- tagfilter_placement()
-#category <- "13caritas"; original <- tagfilter_charity()
-#category <- "14finanz"; original <- tagfilter_finance()
-#category <- "15lotterie"; original <- tagfilter_lotto()
-#category <- "16transport"; original <- tagfilter_transport()
-#category <- "17auskunft"; original <- tagfilter_info()
-#category <- "18uneindeutig"; original <- tagfilter_other()
-
-
 #preparing corpus of German ads
 avis_1834 <- readtext("data/avis_1834.csv",
                       text_field = "text", encoding = "UTF-8")
@@ -48,6 +25,27 @@ corpus_1834 <- corpus_subset(corpus_1834_all,
                                 ids_by_lang$de))
 
 
+#' PICK category and filter by removing # from the pertinent line:
+#category <- "01textilien"; original <- tagfilter_textiles()
+#category <- "02hausrat"; original <- tagfilter_household_goods()
+#category <- "03lebensmittel"; original <- tagfilter_grocery()
+#category <- "04schmuck"; original <- tagfilter_bling()
+#category <- "05drucksachen"; original <- tagfilter_print()
+#category <- "06ding"; original <- tagfilter_things()
+#category <- "07tier"; original <- tagfilter_animal()
+category <- "08immo"; original <- tagfilter_real_estate()
+#category <- "09kirchenstuhl"; original <- tagfilter_churchseat()
+#category <- "arbeit"; original <- tagfilter_labor()
+#category <- "11kost"; original <- tagfilter_board()
+#category <- "12platzierung"; original <- tagfilter_placement()
+#category <- "13caritas"; original <- tagfilter_charity()
+#category <- "14finanz"; original <- tagfilter_finance()
+#category <- "15lotterie"; original <- tagfilter_lotto()
+#category <- "16transport"; original <- tagfilter_transport()
+#category <- "17auskunft"; original <- tagfilter_info()
+#category <- "18uneindeutig"; original <- tagfilter_other()
+
+
 
 #' Testing the impact of new entries
 #'
@@ -56,7 +54,7 @@ corpus_1834 <- corpus_subset(corpus_1834_all,
 tagfilter_test <- function(){
   dict <- list()
   dict$pos <- list(
-    candidate = "Kostgeld"
+    candidate = "Gebäude"
   )
   #take over dict$neg from original filter to better gauge potential of candidate
   #disable by adding # at beginning of next line
@@ -69,7 +67,7 @@ source("R/tagfilters_main.R", encoding = "UTF-8")
 
 
 #' merging original and test filter and prepare validation
-tagfilter_new <- merge_filters(tagfilter_board(),
+tagfilter_new <- merge_filters(original,
                                tagfilter_test())
 new_ids <- tagfilter_new$filtrate(corpus_1834,ignore.case = F)
 n <- validate_filter(corpus_1834, new_ids,
