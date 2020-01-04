b1e571a2cfc3d82c4740f27b6dcd2a8ed457b37c
mbannert <matthias.bannert@gmail.com>
2020-01-05 00:03:34 +0100

    introduced new filter group: practices

---
+++ R/tagfilters_practices.R
@@ -0,0 +1,47 @@
+#' Dictionary spinning
+#' @export
+tagfilter_spinning <- function(){
+  dict <- list()
+  dict$pos <- list(
+    spin = "spin"
+  )
+
+  create_filter_output(dict)
+}
+
+#' Dictionary weaving
+#' @export
+tagfilter_weaving <- function(){
+  dict <- list()
+  dict$pos <- list(
+    weave = "weben"
+  )
+
+  create_filter_output(dict)
+}
+
+
+#' Dictionary cook
+#' @export
+tagfilter_cooking <- function(){
+  dict <- list()
+  dict$pos <- list(
+    cook_1 = "kochen"
+  )
+
+  create_filter_output(dict)
+}
+
+
+
+#' Dictionary cook
+#' @export
+tagfilter_knitting <- function(){
+  dict <- list()
+  dict$pos <- list(
+    knit = "stricken"
+  )
+
+  create_filter_output(dict)
+}
+
