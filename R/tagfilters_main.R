b6036252cc827e719337702aee59e5d6e7fc108a
wissen_ist_acht <ina.serif@unibas.ch>
2020-02-14 14:04:59 +0100

    printfilter

--- R/tagfilters_main.R
+++ R/tagfilters_main.R
@@ -312,12 +312,39 @@ tagfilter_finance <- function(){
 tagfilter_print <- function(){
   dict <- list()
   dict$pos <- list(
-
+    book = "Buch|Bücher[n]|Bucher",
+    edition = "Auflage|Ausgabe|Prachtausgabe|Bdchen",
+    material = "gedruckt|Pergament",
+    person = "Buchhändler|Buchdrucker|Buchbinder",
+    place = "Buchhand*|Buchdruckere[y|i]|Buchladen|Leihbibl*|Lesegesellschaft|Leseanstalt",
+    format_1 = "in Fol.",
+    format_2 = "in 4to.",
+    format_3 = "4°|8°|tom.$|[O|o]ctavo|Bogen|Bögen|Halbfranzband|[ein|un]gebunden|brosch[.|iert]",
+    format_4 = "in [1-9] Bänden",
+    format_5 = "gedruckte[n] Fortsetzung",
+    ausstattung = "Kupf[f]er|Holzschnitt*|Stahlstich",
+    catalog = "Catalogus|Katalog",
+    participant = "Mithalte*|Pr[ae|ä]numerant[en]|Abonnent*",
+    types = "Wörterbuch|Zeitung|Zeitschrift",
+    type = "Neueste Schriften",
+    title_1 = "Rauracher|Rau-racher|Raura-cher",
+    title_2 = "Allgemeine[n] Zeitung",
+    title_3 = "Christliche[r|n] Volksbote*",
+    title_4 = "Kantonsblatt|Kantons-blatt",
+    title_5 = "Annalen",
+    title_6 = "Missions-Magazin",
+    title_7 = "Basler-Zeitung|Basler Zeitung",
+    title_8 = "Wochenblatt"
   )
   dict$neg <- list(
 
+    lost_prayerbooks = "Psalmbuch|[g|G]esangbuch",
+    region = "Entlibuch|Schönenbuch",
+    bible = "Buch Mose",
+    other = "Haushaltungsbuch|Buchstabe|Buchhaltung|Buchsbaum"
   )
   create_filter_output(dict)
+
 }
 
 
