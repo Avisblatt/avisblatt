df4c0177f6365740d3d20236c561ece2e2f2a361
aengel17 <49908904+aengel17@users.noreply.github.com>
2020-02-15 17:47:04 +0100

    NEW: groundtruth1734
    - added groundtruth1734.csv
    -
     adapted and pasted language detection into new script, ran it -> new ids_by_lang.json
    
    changed filter testing script to accomodate for different groundtruths

--- demos/sandbox_testing_new_dict_entries.R
+++ demos/sandbox_testing_new_dict_entries.R
@@ -13,21 +13,25 @@ source("R/tagfilters_household.R", encoding = "UTF-8")
 source("R/tagfilters_main.R", encoding = "UTF-8")
 source("R/cleaners.R")
 source("R/validate_filters.R")
-#preparing corpus of German ads
-avis_1834 <- readtext("data/avis_1834.csv",
+
+
+# preparing corpus of German ads
+# chose year by modifying csv-file name in line below. Currently possible:
+# groundtruth1734.csv
+# groundtruth1834.csv
+# groundtruth*.csv for all
+groundtruth <- readtext("data/groundtruth*.csv",
                       text_field = "text", encoding = "UTF-8")
-avis_1834$text <- correct_ocr(avis_1834$text)
+groundtruth$text <- correct_ocr(groundtruth$text)
 ids_by_lang <- fromJSON("data/ids_by_lang.json")
-corpus_1834_all <- corpus(avis_1834,
+corpus_groundtruth_all <- corpus(groundtruth,
                           docid_field = "doc_id")
-corpus_1834 <- corpus_subset(corpus_1834_all,
-                             (docvars(corpus_1834_all,"id") %in%
+corpus_groundtruth <- corpus_subset(corpus_groundtruth_all,
+                             (docvars(corpus_groundtruth_all,"id") %in%
                                 ids_by_lang$de))
-
-
-#' PICK category and filter by removing # from the pertinent line below
-#' #reload current version of filters here, so lines ~28-100
-#' #can be executed as a block after changing original and test dictionary
+# PICK category and filter by removing # from the pertinent line below
+# reload current version of filters here, so lines ~28-100
+# can be executed as a block after changing original and test dictionary
 source("R/tagfilters_main.R", encoding = "UTF-8")
 #category <- "01textilien"; original <- tagfilter_textiles()
 #category <- "02hausrat"; original <- tagfilter_household_goods()
@@ -38,8 +42,8 @@ source("R/tagfilters_main.R", encoding = "UTF-8")
 #category <- "07tier"; original <- tagfilter_animal()
 #category <- "08immo"; original <- tagfilter_real_estate()
 #category <- "09kirchenstuhl"; original <- tagfilter_churchseat()
-#category <- "arbeit"; original <- tagfilter_labor()
-category <- "11kost"; original <- tagfilter_board()
+category <- "10arbeitsstelle"; original <- tagfilter_labor()
+#category <- "11kost"; original <- tagfilter_board()
 #category <- "12platzierung"; original <- tagfilter_placement()
 #category <- "13caritas"; original <- tagfilter_charity()
 #category <- "14finanz"; original <- tagfilter_finance()
@@ -51,8 +55,8 @@ category <- "11kost"; original <- tagfilter_board()
 
 #' Examining current filter
 #'
-original_ids <- original$filtrate(corpus_1834,ignore.case = F)
-o <- validate_filter(corpus_1834, original_ids,
+original_ids <- original$filtrate(corpus_groundtruth,ignore.case = F)
+o <- validate_filter(corpus_groundtruth, original_ids,
                      search_col = "adcontent",
                      pattern <- category)
 o
@@ -76,14 +80,14 @@ tagfilter_test <- function(){
 #' merging original and test filter and prepare validation
 tagfilter_new <- merge_filters(original,
                                tagfilter_test())
-new_ids <- tagfilter_new$filtrate(corpus_1834,ignore.case = F)
-n <- validate_filter(corpus_1834, new_ids,
+new_ids <- tagfilter_new$filtrate(corpus_groundtruth,ignore.case = F)
+n <- validate_filter(corpus_groundtruth, new_ids,
                      search_col = "adcontent",
                      pattern <- category)
 
 test <- tagfilter_test()
-test_ids <- test$filtrate(corpus_1834,ignore.case = F)
-t <- validate_filter(corpus_1834, test_ids,
+test_ids <- test$filtrate(corpus_groundtruth,ignore.case = F)
+t <- validate_filter(corpus_groundtruth, test_ids,
                      search_col = "adcontent",
                      pattern <- category)
 
@@ -108,14 +112,14 @@ cat(paste("Range (%):\t", o$range, "->", n$range, "| change:", round (n$range-o$
 #'
 #' TRUE positives ("yay!") of test
 #' ...useful if one checks for entries who seem more fordict$neg
-yay <- corpus_subset(corpus_1834,
-                     docvars(corpus_1834,"id") %in%
+yay <- corpus_subset(corpus_groundtruth,
+                     docvars(corpus_groundtruth,"id") %in%
                        t$filter_T_hc_T)
 yay$documents$texts[1:10]
 
 #- FALSE positives ("oops") of test
 oops <- corpus_subset(corpus_1834,
-                      docvars(corpus_1834,"id") %in%
+                      docvars(corpus_groundtruth,"id") %in%
                         t$filter_T_hc_F)
 oops$documents$texts[1:10]
 
@@ -124,14 +128,14 @@ oops$documents$texts[1:10]
 #' ---original
 #'
 #' FALSE positives ("oops") of original
-oops <- corpus_subset(corpus_1834,
-                    docvars(corpus_1834,"id") %in%
+oops <- corpus_subset(corpus_groundtruth,
+                    docvars(corpus_groundtruth,"id") %in%
                       o$filter_T_hc_F)
 oops$documents$texts[1:10]
 
 #' FALSE negatives ("What are we still missing?") of original
-missing_corpus <- corpus_subset(corpus_1834,
-                                docvars(corpus_1834,"id") %in%
+missing_corpus <- corpus_subset(corpus_groundtruth,
+                                docvars(corpus_groundtruth,"id") %in%
                                   o$hc_T_filter_F)
 missing_corpus_clean <- missing_corpus %>%
   tokens(remove_punct = TRUE,
@@ -151,14 +155,14 @@ head(kwic(missing_corpus, pattern = "something"))
 #' ---merged/new
 #'
 #' FALSE positives ("oops") of merged/new
-oops <- corpus_subset(corpus_1834,
-                    docvars(corpus_1834,"id") %in%
+oops <- corpus_subset(corpus_groundtruth,
+                    docvars(corpus_groundtruth,"id") %in%
                       n$filter_T_hc_F)
 oops$documents$texts[1:10]
 
 #' FALSE negatives ("What are we still missing?") of merged/new
-missing_corpus <- corpus_subset(corpus_1834,
-                                docvars(corpus_1834,"id") %in%
+missing_corpus <- corpus_subset(corpus_groundtruth,
+                                docvars(corpus_groundtruth,"id") %in%
                                   n$hc_T_filter_F)
 missing_corpus_clean <- missing_corpus %>%
   tokens(remove_punct = TRUE,
