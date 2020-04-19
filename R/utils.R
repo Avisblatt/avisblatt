c903d254858f9cb770ccc12e95671f0d37c28180
Matt Bannert <matthias.bannert@gmail.com>
2020-04-19 17:58:00 +0200

    function to purge i n t e r r u p te d print

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,22 @@ get_subcorpus_by_id <- function(corp, ids, idvar = "id"){
 }
 
 
+#' Turn Interrupted Print into Regular Print
+#'
+#' Turn s p a c e into space.
+#'
+#' @param txt character string
+#' @examples
+#' s <- "c o o l e r than a polar b e a r's toe n a i l s."
+#' purge_s_p_a_c_e(s)
+#' @export
+purge_s_p_a_c_e <- function(txt){
+  splits <- strsplit(txt, "\\s")[[1]]
+  more_than_2 <- grepl("\\w{2,}", splits)
+  tf <- more_than_2 | c(FALSE, more_than_2[1:(length(more_than_2)-1)])
+  # cumsum is an elegant way to keep track of T/F swaps
+  out <- split(splits, cumsum(tf))
+  # reconstruct
+  paste(sapply(out, paste, collapse = ""), collapse = " ")
+}
+
