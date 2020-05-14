62d902fd03e82293a3f24133b49f087f6b5e1fa2
Matt Bannert <matthias.bannert@gmail.com>
2020-05-14 22:23:39 +0200

    just avoid stashing when switching branches, clean up later

--- demos/mba_dev_sandbox.R
+++ demos/mba_dev_sandbox.R
@@ -1,3 +1,90 @@
+#
+#'@export
+AvisCollection <- R6Class("AvisCollection",list(
+  corpus = NULL,
+  meta = NULL,
+  record_count = NULL,
+  initialize = function(crps,
+                        meta_list = NULL,
+                        date = NULL){
+    # either specify path to avisblatt type of .csv
+    # quanteda corpus object
+    if(inherits(crps,"corpus")){
+      self$corpus <- crps
+    } else if(is.character(crps)){
+      self$corpus <- avis_create_corpus(crps)
+    } else{
+      stop("unsupported class. collections can only be initialized from Corpus or links to
+           .csvs containing corpora.")
+    }
+
+    if(is.null(meta_list)){
+      l <- lapply(names(self$corpus), function(x){
+        MetaInfoRecord$new(id = x, date)
+      })
+      names(l) <- names(self$corpus)
+      self$meta <- list2env(l)
+    } else{
+      l <- lapply(meta_list, function(x){
+        MetaInfoRecord$new(id = x$id,
+                           tag = x$tag,
+                           date = x$date,
+                           language = x$language
+        )
+      })
+      names(l) <- names(meta_list)
+      self$meta <- list2env(l)
+    }
+  },
+  count_records_in_collect = function(){
+    length(self$corpus)
+  },
+  bulk_update_tags = function(ids = NULL, tags){
+    if(is.null(ids)) {
+      list_of_env <- mget(ls(self$meta),
+                          self$meta)
+    } else{
+      list_of_env <- mget(ids, self$meta)
+    }
+
+    names(list_of_env) <- ids
+    lapply(list_of_env, function(x) x$add_tag(tags))
+    invisible(self)
+  },
+  bulk_update_language = function(ids = NULL, lang){
+    if(is.null(ids)) {
+      list_of_env <- mget(ls(self$meta),
+                          self$meta)
+    } else{
+      list_of_env <- mget(ids, self$meta)
+    }
+
+    names(list_of_env) <- ids
+    lapply(list_of_env, function(x) x$set_language(lang))
+    invisible(self)
+  },
+  show_distinct_tags = function(){
+    l <- unlist(eapply(self$meta, function(x) x$tags),
+                recursive = F)
+    unique(l)
+  },
+  search_tags = function(search, regex = FALSE){
+    if(regex){
+      e <- eapply(self$meta, function(x){
+        any(grepl(search, x$tags))
+      })
+    } else{
+      e <- eapply(self$meta, function(x){
+        any(search %in% x$tags)
+      })
+    }
+    names(e)[unlist(e)]
+  }
+
+))
+
+
+
 # idea avis collection object
 avis_1834 <- AvisCollection$new("../data/raw_data/orig_1834.csv", year = 1834)
 class(avis_1834)
@@ -11,8 +98,8 @@ avis_1834$corpus['0066a6d4-fcaf-5b7d-b7aa-68e3d971725d/a1']
 avis_1834$bulk_update_language(lang = "de")
 
 avis_1834$count_records_in_collect()
-
 ids <- ls(avis_1834$meta)[1:3]
+
 
 avis_1834$bulk_update_tags(ids,tags = c("more","please"))
 
--- demos/store_meta.R
+++ demos/store_meta.R
@@ -1,118 +1,7 @@
 library(jsonlite)
 library(R6)
 
-MetaInfoRecord <- R6Class("MetaInfoRecord", list(
-  id = NULL,
-  year = NULL,
-  language = NULL,
-  date = NULL,
-  tags = NULL,
-  initialize = function(id,
-                        year = NULL,
-                        date = NULL,
-                        tags = NULL,
-                        language = NULL){
-    self$id <-id
-    self$year <- year
-    self$date <- date
-    self$tags <- tags
-    self$language <- tags
-  },
-  add_tag = function(tags){
-    self$tags <- unique(c(self$tags, tags))
-    invisible(self)
-  },
-  overwrite_tag = function(tags){
-    self$tags <- unique(tags)
-  },
-  set_language = function(language){
-    self$language <- unique(language)
-  }
-))
 
-AvisCollection <- R6Class("AvisCollection",list(
-  corpus = NULL,
-  meta = NULL,
-  record_count = NULL,
-  initialize = function(crps,
-                        meta_list = NULL,
-                        year = NULL){
-    # either specify path to avisblatt type of .csv
-    # quanteda corpus object
-    if(inherits(crps,"corpus")){
-      self$corpus <- crps
-    } else if(is.character(crps)){
-      self$corpus <- avis_create_corpus(crps)
-    } else{
-      stop("unsupported class. collections can only be initialized from Corpus or links to
-           .csvs containing corpora.")
-    }
-
-    if(is.null(meta_list)){
-      l <- lapply(names(self$corpus), function(x){
-        MetaInfoRecord$new(id = x, year)
-      })
-      names(l) <- names(self$corpus)
-      self$meta <- list2env(l)
-    } else{
-      l <- lapply(meta_list, function(x){
-        MetaInfoRecord$new(id = x$id,
-                           tag = x$tag,
-                           date = x$date,
-                           language = x$language,
-                           year = x$year
-                           )
-      })
-      names(l) <- names(meta_list)
-      self$meta <- list2env(l)
-    }
-  },
-  count_records_in_collect = function(){
-    length(self$corpus)
-  },
-  bulk_update_tags = function(ids = NULL, tags){
-    if(is.null(ids)) {
-      list_of_env <- mget(ls(self$meta),
-                          self$meta)
-    } else{
-      list_of_env <- mget(ids, self$meta)
-    }
-
-    names(list_of_env) <- ids
-    lapply(list_of_env, function(x) x$add_tag(tags))
-    invisible(self)
-  },
-  bulk_update_language = function(ids = NULL, lang){
-    if(is.null(ids)) {
-      list_of_env <- mget(ls(self$meta),
-                          self$meta)
-    } else{
-      list_of_env <- mget(ids, self$meta)
-    }
-
-    names(list_of_env) <- ids
-    lapply(list_of_env, function(x) x$set_language(lang))
-    invisible(self)
-  },
-  show_distinct_tags = function(){
-    l <- unlist(eapply(self$meta, function(x) x$tags),
-                recursive = F)
-    unique(l)
-  },
-  search_tags = function(search, regex = FALSE){
-    if(regex){
-      e <- eapply(self$meta, function(x){
-        any(grepl(search, x$tags))
-      })
-    } else{
-      e <- eapply(self$meta, function(x){
-        any(search %in% x$tags)
-      })
-    }
-    names(e)[unlist(e)]
-  }
-
-))
 
 
 
@@ -140,7 +29,7 @@ write_collection <- function(x,
   # data slots need to be updated when collection class
   # changes as this set helps to distinguish non-data slots
   # such as functions from data slots...
-  data_slots <- c("id","tags","year","date","language")
+  data_slots <- c("id","tags","date","language")
   li <- lapply(ee, function(e){
     n <- names(e)
     sel <- n[n %in% data_slots]
