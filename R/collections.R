bcaa950cabd794c6d8bc704acc18863c7b219e99
Matt Bannert <matthias.bannert@gmail.com>
2020-05-16 12:38:05 +0200

    experimental support of information coming from docvars, i.e., groundtruth vars (manual tags)

--- R/collections.R
+++ R/collections.R
@@ -4,14 +4,17 @@ MetaInfoRecord <- R6Class("MetaInfoRecord", list(
   date = NULL,
   language = NULL,
   tags = NULL,
+  tags_manual = NULL,
   initialize = function(id,
                         date = NULL,
                         tags = NULL,
+                        tags_manual = NULL,
                         language = NULL){
     self$id <-id
     self$date <- date
     self$tags <- tags
-    self$language <- tags
+    self$tags_manual <- tags_manual
+    self$language <- language
   },
   add_tag = function(tags){
     self$tags <- unique(c(self$tags, tags))
@@ -27,13 +30,17 @@ MetaInfoRecord <- R6Class("MetaInfoRecord", list(
 
 
 #'@export
-AvisCollection <- R6Class("AvisCollection",list(
+AvisCollection <- R6Class("AvisCollection", list(
   corpus = NULL,
   meta = NULL,
   record_count = NULL,
+  docvars_to_meta = NULL,
+  transform_docvars = NULL,
   initialize = function(crps,
                         meta_list = NULL,
-                        date = NULL){
+                        date = NULL,
+                        docvars_to_meta = NULL,
+                        transform_docvars = NULL){
     # either specify path to avisblatt type of .csv
     # quanteda corpus object
     if(inherits(crps,"corpus")){
@@ -45,19 +52,55 @@ AvisCollection <- R6Class("AvisCollection",list(
            .csvs containing corpora.")
     }
 
+    if(!is.null(docvars_to_meta)){
+      # find out which columns to keep, cause
+      # we want only to remove the information from corpus
+      # will be transferred to meta information
+      keep_cols <- setdiff(names(docvars(self$corpus)), docvars_to_meta)
+      dv_list <- as.list(docvars(self$corpus)[,docvars_to_meta])
+      attr(self$corpus, "docvars") <- docvars(self$corpus)[, keep_cols]
+
+
+      # TODO discuss whether we want to
+      # keep this consolidation of meta information
+      # the alternative would be a nested list
+      if(length(docvars_to_meta) > 1){
+        dv_list <- as.list(do.call("paste", c(dv_list, sep = ",")))
+      }
+
+      if(!is.null(transform_docvars)){
+        dv_list <- lapply(dv_list, transform_docvars)
+      }
+
+      names(dv_list) <- names(self$corpus)
+    }
+
+
     if(is.null(meta_list)){
       l <- lapply(names(self$corpus), function(x){
-        MetaInfoRecord$new(id = x, date)
+        # avoid errors because of non-existing docvar
+        # transformation which might be the case most
+        # of the time
+        if(exists("dv_list")){
+          dv_vec <- unlist(dv_list[x])
+          # remove vector names as idx based on vector length
+          # is attached and be confusing w/o any added value
+          names(dv_vec) <- NULL
+          MetaInfoRecord$new(id = x, date,
+                             tags_manual = dv_vec)
+        } else {
+          MetaInfoRecord$new(id = x, date)
+        }
       })
       names(l) <- names(self$corpus)
       self$meta <- list2env(l)
     } else{
       l <- lapply(meta_list, function(x){
         MetaInfoRecord$new(id = x$id,
-                           tag = x$tag,
+                           tags = x$tag,
                            date = x$date,
-                           language = x$language
-        )
+                           language = x$language,
+                           tags_manual = x$tags_manual)
       })
       names(l) <- names(meta_list)
       self$meta <- list2env(l)
@@ -90,19 +133,25 @@ AvisCollection <- R6Class("AvisCollection",list(
     lapply(list_of_env, function(x) x$set_language(lang))
     invisible(self)
   },
-  show_distinct_tags = function(){
-    l <- unlist(eapply(self$meta, function(x) x$tags),
-                recursive = F)
+  show_distinct_tags = function(manual = FALSE){
+    if(manual){
+      l <- unlist(eapply(self$meta, function(x) x$tags_manual),
+                  recursive = F)
+    } else{
+      l <- unlist(eapply(self$meta, function(x) x$tags),
+                  recursive = F)
+    }
+
     unique(l)
   },
-  search_tags = function(search, regex = FALSE){
+  search_tags = function(search, regex = FALSE, manual = FALSE){
     if(regex){
       e <- eapply(self$meta, function(x){
-        any(grepl(search, x$tags))
+        any(grepl(search, ifelse(manual,x$tags_manual,x$tags)))
       })
     } else{
       e <- eapply(self$meta, function(x){
-        any(search %in% x$tags)
+        any(search %in% ifelse(manual,x$tags_manual,x$tags))
       })
     }
     names(e)[unlist(e)]
