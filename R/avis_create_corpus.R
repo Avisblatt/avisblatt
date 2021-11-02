6f219670c6ed19c5643c040eac730dc24d331540
aengel17 <49908904+aengel17@users.noreply.github.com>
2021-11-02 18:44:13 +0100

    Accidently removed avis_create_corpus, bringing it back here

---
+++ R/avis_create_corpus.R
@@ -0,0 +1,24 @@
+#' Reads Avisblatt Input CSV into Quantedata Corpus
+#'
+#' Covenience function to read avisblatt input .csvs into
+#' quanteda 2.0 corpuses.
+#'
+#' @importFrom data.table fread
+#' @importFrom quanteda corpus
+#' @export
+avis_create_corpus <- function(f,
+                               text_field = "text",
+                               docid_field = "id",
+                               encoding = "UTF-8",
+                               apply_ocr_correct = FALSE){
+  txt <- fread(f,
+               encoding = encoding)
+
+  if(apply_ocr_correct){
+    txt$text <- correct_ocr(txt$text)
+  }
+
+  corpus(txt,
+         text_field = text_field,
+         docid_field = docid_field)
+}
