5ffe940078e8b79076b69157f7ff0d39ae375491
wissen-ist-acht <49908922+wissen-ist-acht@users.noreply.github.com>
2021-12-06 12:57:13 +0100

    Create tag_by_regex.R

---
+++ R/tag_by_regex.R
@@ -0,0 +1,33 @@
+# match regex in data frame consisting of two columns:
+# 1: tags, 2: regex
+# with text in ad and assigns corresponding tag
+# input: corpus, data frame
+# output: list of tags, named by corresponding ad id
+identify_tags <- function(ad,df_tags){
+  adtext <- as.character(ad)
+  tags <- df_tags[which(grepl_vect(df_tags[,2],adtext)),1]
+  return(tags)
+}
+
+
+# appends tags as defined in taglist to the elements of a corpus
+# input:  corpus
+#         taglist - list of tags to append to ads named by id
+# output: corpus
+append_tags <- function(corpus,taglist){
+  for(iTag in 1:length(taglist)){
+    sId <- names(taglist[iTag])
+    vTags <- corpus$meta[corpus$meta$id == sId,]$tags[[1]]
+    corpus$meta[corpus$meta$id == sId,]$tags <- c(vTags,taglist[[iTag]])
+  }
+  return(corpus)
+}
+
+# append tags in a corpus by matching regex in data frame with ad text
+# input: corpus
+#        data frame, consisting of two columns:  1: tags, 2: regex
+# output: corpus
+
+tag_by_regex <- function(corpus,df_tags){
+  return(append_tags(corpus,identify_tags(corpus,df_tags)))
+}
