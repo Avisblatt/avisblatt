b09276a439eeda61578f973c53b5b14ed047bfd7
aengel17 <49908904+aengel17@users.noreply.github.com>
2023-05-11 08:49:25 +0200

    Optimized reprint detetction

--- R/data_processing_flagging.R
+++ R/data_processing_flagging.R
@@ -47,54 +47,27 @@ rawdata_reprint_detection <- function(AVIS_YEARS = 1729:1844,
   current_year <- as.numeric(substr(issuedates[1], 1, 4)) #just for messaging
   ti <- Sys.time()
   for (j in 2:length(issuedates)){
-    # account for missing years:
-    # do not compare issues further than 10 days apart
-    # (10 instead of 7 because issue dates can move 1-2 days)
     if (difftime(issuedates[j-1], issuedates[j]) < 10){
       s <- advert_distance(ci[[j]], ci[[j-1]], consider_length_diff = TRUE)
-      # For each current ad, chose the most similar ad from previous week, and vice versa
       r_current  <- as.matrix(apply(s, 1, function(x) {colnames(s)[which.min(x)]}))
       r_previous <- as.matrix(apply(s, 2, function(x) {rownames(s)[which.min(x)]}))
-      # Attach the similarity to it
       r_current  <- cbind.data.frame(r_current,  round(apply(s, 1, min), 4))
       r_previous <- cbind.data.frame(r_previous, round(apply(s, 2, min), 4))
-      # name columns to avoid name matching problems in rbind further down
       colnames(r_current)  <- c("id1", "dist1")
       colnames(r_previous)  <- c("id1", "dist1")
-      # From August 1838, there was more than one issue per week
-      # In some cases, reprints then skipped an issue.
-      # So we also need to check one issue further
-      # As we concurrently look for
-      # 1. a potential original ad of each ad in the current issue
-      # 2. a potential reprint of each ad in the previous issue,
-      # this needs to be done in two parts:
-      # FIRST, for each ad in current issue,
-      # determine potential original ad in issue before previous issue
       if (j > 2 & issuedates[j] > "1838-08-01"){
         s <- advert_distance(ci[[j]], ci[[j-2]], consider_length_diff = TRUE)
-        # temporarily extend results for neighbouring issues
-        # by results from comparing issues one apart
         r_current  <- cbind.data.frame(r_current,
                                        as.matrix(apply(s, 1, function(x) {colnames(s)[which.min(x)]})),
                                        round(apply(s, 1, min), 4))
         colnames(r_current)  <- c("id1", "dist1", "id2", "dist2")
-        # For each ad,
-        # id1 denotes most similiar ad from previous issue,
-        # id2 most similiar ad from issue before that.
-        # Now substitute id1 by id2 if and only if
-        # - id1 does appear to not be a reprint/original
-        #    (i.e. if dist1 > 1)
-        # - id2 has a higher similarity (i.e. dist2 < dist1)
         r_current$id1    <- ifelse(r_current$dist1 > 1 & (r_current$dist2 < r_current$dist1),
                                    as.vector(r_current$id2), as.vector(r_current$id1))
         r_current$dist1  <- ifelse(r_current$dist1 > 1 & (r_current$dist2 < r_current$dist1),
                                    r_current$dist2, r_current$dist1)
-        # remove temporary columns
         r_current$id2    <- NULL
         r_current$dist2  <- NULL
       }
-      # SECOND, for each ad in previous issue,
-      # determine potential reprint in issue AFTER current issue
       if (j < length(issuedates) & issuedates[j] > "1838-08-01"){
         s <- advert_distance(ci[[j+1]], ci[[j-1]], consider_length_diff = TRUE)
         r_previous <- cbind.data.frame(r_previous,
@@ -119,6 +92,7 @@ rawdata_reprint_detection <- function(AVIS_YEARS = 1729:1844,
       }
     }
   }
+  
   message(paste0("Reprints detected for all ads from ", current_year, ", took ", itime, " minutes."))
   
   # attach results and reprint_status column to meta data
@@ -146,6 +120,21 @@ rawdata_reprint_detection <- function(AVIS_YEARS = 1729:1844,
   message("\nWrite metadata to disc, year-by-year:")
   write_metadata(meta_dt, dest_path)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
